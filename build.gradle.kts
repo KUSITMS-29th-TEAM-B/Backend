ace5e2f9c034f78a76717e4378303dc7bbeb64c5
whereami0404 <whereami2048@gmail.com>
2024-05-01 04:00:48 +0900

    chore: 스프링 부트 버전 다운그레이드 3.2.5 -> 3.1.1(#7)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("java-test-fixtures")
-    id("org.springframework.boot") version "3.2.5"
+    id("org.springframework.boot") version "3.1.1"
     id("io.spring.dependency-management") version "1.1.4"
 
     kotlin("jvm") version "1.9.23"
