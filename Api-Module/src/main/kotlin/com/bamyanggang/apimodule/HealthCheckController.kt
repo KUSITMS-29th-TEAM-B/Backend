75d3c91017d1143853f1ec1e5dc432da633ad428
whereami0404 <whereami2048@gmail.com>
2024-05-02 15:28:19 +0900

    chore: CD 테스트를 위한 healthCheckController 임시 추가

---
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/HealthCheckController.kt
@@ -0,0 +1,12 @@
+package com.bamyanggang.apimodule
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckController {
+    @GetMapping("")
+    fun healthCheck(): String {
+        return "OK"
+    }
+}
