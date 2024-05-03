6fc250481eb2fbe4cc6fc5cb4cfca1efabd3d251
seeunjang <wisjang1@naver.com>
2024-05-03 17:12:29 +0900

    refactor : 의존성 수정(#4)

--- Api-Module/build.gradle.kts
+++ Api-Module/build.gradle.kts
@@ -1,16 +1,12 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-    // Feign
-    implementation("io.github.openfeign:feign-httpclient:12.1")
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.3")
-
-    // JWT
-    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-    implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
-    implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+    // webclient
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 
     //OAuth
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
+    implementation(project(":Domain-Module"))
+    implementation(project(":Common-Module"))
 }
