d2c689ced58fc7b0422d35c34a803cb3a02854a6
whereami0404 <whereami2048@gmail.com>
2024-05-07 18:52:59 +0900

    chore: blue-green 배포 방식 변경에 따른 CD-dev 워크 플로우 파일 수정(#17)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/auth/application/service/command/AuthInvoker.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/auth/application/service/command/AuthInvoker.kt
@@ -13,21 +13,22 @@ import org.springframework.stereotype.Component
 class AuthInvoker (
     private val authHandlerList: List<AuthHandler>,
     private val jwtProvider: JwtProvider,
-    private val userReader: UserReader
+//    private val userReader: UserReader
     ) {
 
     fun invoke(request: SocialLogin.Request, provider: SocialLoginProvider): SocialLogin.Response {
         val response = attemptLogin(request, provider)
-        return TransactionUtils.writable {
-            return@writable userReader.findBySocialIdOrNull(response.socialId)?.let {
-                val accessToken = jwtProvider.generateAccessToken(Claims.UserClaims(it.id))
-                val refreshToken = jwtProvider.generateRefreshToken(Claims.UserClaims(it.id))
-                SocialLogin.Response.Success(accessToken, refreshToken)
-            } ?: run {
-                val registrationToken = jwtProvider.generateRegistrationToken(Claims.RegistrationClaims(response.socialId, provider))
-                SocialLogin.Response.UnRegistered(registrationToken, response.nickName)
-            }
-        }
+        return SocialLogin.Response.Success("accessToken", "refreshToken")
+//        return TransactionUtils.writable {
+//            return@writable userReader.findBySocialIdOrNull(response.socialId)?.let {
+//                val accessToken = jwtProvider.generateAccessToken(Claims.UserClaims(it.id))
+//                val refreshToken = jwtProvider.generateRefreshToken(Claims.UserClaims(it.id))
+//                SocialLogin.Response.Success(accessToken, refreshToken)
+//            } ?: run {
+//                val registrationToken = jwtProvider.generateRegistrationToken(Claims.RegistrationClaims(response.socialId, provider))
+//                SocialLogin.Response.UnRegistered(registrationToken, response.nickName)
+//            }
+//        }
     }
 
     private fun attemptLogin(request: SocialLogin.Request, provider: SocialLoginProvider): AuthHandler.Response {
