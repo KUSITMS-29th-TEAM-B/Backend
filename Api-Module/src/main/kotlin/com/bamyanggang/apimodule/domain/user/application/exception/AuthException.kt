175e744cfbaa9b30b5bc704345874e4a93391c37
seeunjang <wisjang1@naver.com>
2024-05-09 04:12:20 +0900

    refactor : AuthException errodCode 정보 수정(#14)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/user/application/exception/AuthException.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/user/application/exception/AuthException.kt
@@ -1,4 +1,4 @@
-package com.bamyanggang.apimodule.domain.auth.application.exception
+package com.bamyanggang.apimodule.domain.user.application.exception
 
 import com.bamyanggang.commonmodule.exception.CustomException
 import org.springframework.http.HttpStatus
@@ -11,13 +11,13 @@ sealed class AuthException (
 ) : CustomException(CODE_PREFIX, errorCode, httpStatusCode , message) {
 
         class OAuthFailed(message: String = "OAuth 인증에 실패하였습니다.") :
-            AuthException(errorCode = 1001, httpStatusCode = HttpStatus.BAD_REQUEST, message = message)
+            AuthException(errorCode = 1, httpStatusCode = HttpStatus.BAD_REQUEST, message = message)
             
         class KakaoUserInfoRetrievalException(message: String = "카카오 사용자 정보를 가져오는데 실패했습니다.") :
-            AuthException(errorCode = 1002, httpStatusCode = HttpStatus.BAD_REQUEST, message = message)
+            AuthException(errorCode = 2, httpStatusCode = HttpStatus.BAD_REQUEST, message = message)
 
         class GoogleUserInfoRetrievalException(message: String = "구글 사용자 정보를 가져오는데 실패했습니다.") :
-            AuthException(errorCode = 1003, httpStatusCode = HttpStatus.BAD_REQUEST, message = message)
+            AuthException(errorCode = 3, httpStatusCode = HttpStatus.BAD_REQUEST, message = message)
 
         companion object {
             const val CODE_PREFIX = "AUTH"
