c0dac2bb08fafcfcdae5804006b9ee19020f1539
whereami0404 <whereami2048@gmail.com>
2024-05-11 03:31:35 +0900

    feat: 역량 키워드 엔티티 구현(#23)

---
+++ Infrastructure-Module/src/main/kotlin/com/bamyanggang/infrastructuremodule/persistence/domain/strongpoint/entity/StrongPointJpaEntity.java
@@ -0,0 +1,19 @@
+package com.bamyanggang.infrastructuremodule.persistence.domain.strongpoint.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "strong_point")
+public class StrongPointJpaEntity {
+    @Id
+    private UUID id;
+
+    private String name;
+}
