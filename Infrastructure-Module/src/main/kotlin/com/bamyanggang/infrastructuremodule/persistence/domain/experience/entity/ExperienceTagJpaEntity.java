a9e309c15bba3a3d046e9b0696961eab0244c8b6
whereami0404 <whereami2048@gmail.com>
2024-05-11 03:32:11 +0900

    feat: 경험-태그 중간 엔티티 구현(#23)

---
+++ Infrastructure-Module/src/main/kotlin/com/bamyanggang/infrastructuremodule/persistence/domain/experience/entity/ExperienceTagJpaEntity.java
@@ -0,0 +1,28 @@
+package com.bamyanggang.infrastructuremodule.persistence.domain.experience.entity;
+
+import static jakarta.persistence.FetchType.*;
+
+import com.bamyanggang.infrastructuremodule.persistence.domain.tag.entity.TagJpaEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "experience_tag")
+public class ExperienceTagJpaEntity {
+    @Id
+    private UUID id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "experience_id")
+    private ExperienceJpaEntity experience;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "tag_id")
+    private TagJpaEntity tag;
+}
