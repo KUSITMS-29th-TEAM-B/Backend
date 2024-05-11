59e431177396cf3004a39c1b7d61dfbfac03d612
whereami0404 <whereami2048@gmail.com>
2024-05-11 13:30:29 +0900

    refactor: BaseTimeEntity 엔티티 생성 시간 자동 저장 기능 제거(#23)

--- Infrastructure-Module/src/main/kotlin/com/bamyanggang/infrastructuremodule/persistence/global/BaseTimeEntity.java
+++ Infrastructure-Module/src/main/kotlin/com/bamyanggang/infrastructuremodule/persistence/global/BaseTimeEntity.java
@@ -1,21 +1,14 @@
 package com.bamyanggang.infrastructuremodule.persistence.global;
 
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
-    @CreatedDate
     private LocalDateTime createdAt;
 
-    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
