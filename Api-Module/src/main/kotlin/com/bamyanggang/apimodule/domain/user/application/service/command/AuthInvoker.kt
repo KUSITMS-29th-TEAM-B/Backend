9bb2c95db63a036755564fda67648adafdb9d50c
seeunjang <wisjang1@naver.com>
2024-05-12 15:20:06 +0900

    feat : 로그인 시 refresh token 저장 로직 추가(#11)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/user/application/service/command/AuthInvoker.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/user/application/service/command/AuthInvoker.kt
@@ -5,31 +5,34 @@ import com.bamyanggang.apimodule.domain.user.application.exception.AuthException
 import com.bamyanggang.apimodule.domain.user.application.service.command.handler.AuthHandler
 import com.bamyanggang.commonmodule.util.TransactionUtils
 import com.bamyanggang.domainmodule.domain.user.enums.SocialLoginProvider
+import com.bamyanggang.domainmodule.domain.user.service.TokenAppender
 import com.bamyanggang.domainmodule.domain.user.service.UserReader
 import com.bamyanggang.supportmodule.jwt.Claims
 import com.bamyanggang.supportmodule.jwt.JwtProvider
+import com.bamyanggang.supportmodule.jwt.TokenType
 import org.springframework.stereotype.Component
 
 @Component
 class AuthInvoker (
     private val authHandlerList: List<AuthHandler>,
     private val jwtProvider: JwtProvider,
-    private val userReader: UserReader
+    private val userReader: UserReader,
+    private val tokenAppender: TokenAppender
     ) {
 
     fun invoke(request: SocialLogin.Request, provider: SocialLoginProvider): SocialLogin.Response {
         val response = attemptLogin(request, provider)
-        return SocialLogin.Response.Success("accessToken", "refreshToken")
-//        return TransactionUtils.writable {
-//            return@writable userReader.findBySocialIdOrNull(response.socialId)?.let {
-//                val accessToken = jwtProvider.generateAccessToken(Claims.UserClaims(it.id))
-//                val refreshToken = jwtProvider.generateRefreshToken(Claims.UserClaims(it.id))
-//                SocialLogin.Response.Success(accessToken, refreshToken)
-//            } ?: run {
-//                val registrationToken = jwtProvider.generateRegistrationToken(Claims.RegistrationClaims(response.socialId, provider))
-//                SocialLogin.Response.UnRegistered(registrationToken, response.nickName)
-//            }
-//        }
+        return TransactionUtils.writable {
+            return@writable userReader.readUserBySocialId(response.socialId)?.let {
+                val accessToken = jwtProvider.generateAccessToken(Claims.UserClaims(it.id))
+                val refreshToken = jwtProvider.generateRefreshToken(Claims.UserClaims(it.id))
+                tokenAppender.appendToken(it, refreshToken, TokenType.REFRESH_TOKEN)
+                SocialLogin.Response.Success(accessToken, refreshToken)
+            } ?: run {
+                val registrationToken = jwtProvider.generateRegistrationToken(Claims.RegistrationClaims(response.socialId))
+                SocialLogin.Response.UnRegistered(registrationToken, response.nickName)
+            }
+        }
     }
 
     private fun attemptLogin(request: SocialLogin.Request, provider: SocialLoginProvider): AuthHandler.Response {
