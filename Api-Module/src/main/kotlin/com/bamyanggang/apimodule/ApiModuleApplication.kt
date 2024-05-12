0cd9096abb30c2ee35c9d61fb276b7b9a9d9efe8
seeunjang <wisjang1@naver.com>
2024-05-12 14:56:32 +0900

    refactor : import문 및 config 파일 제거(#11)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/ApiModuleApplication.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/ApiModuleApplication.kt
@@ -1,22 +1,11 @@
 package com.bamyanggang.apimodule
 
-import com.bamyanggang.apimodule.global.config.ComponentScanConfig
-import com.bamyanggang.domainmodule.common.config.DomainConfig
-import com.bamyanggang.infrastructuremodule.persistence.config.PersistenceConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
 
 @SpringBootApplication(
     exclude = [UserDetailsServiceAutoConfiguration::class]
-)
-@Import(
-    value = [
-        ComponentScanConfig::class,
-        DomainConfig::class,
-        PersistenceConfig::class
-    ]
 )
 class ApiModuleApplication {
 }
