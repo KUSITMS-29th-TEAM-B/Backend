b568e71df479bf307ec01dcaffb42ea9d11ca6c9
seeunjang <wisjang1@naver.com>
2024-05-12 13:00:56 +0900

    feat : config 파일들 import(#11)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/ApiModuleApplication.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/ApiModuleApplication.kt
@@ -1,11 +1,22 @@
 package com.bamyanggang.apimodule
 
+import com.bamyanggang.apimodule.global.config.ComponentScanConfig
+import com.bamyanggang.domainmodule.common.config.DomainConfig
+import com.bamyanggang.infrastructuremodule.persistence.config.PersistenceConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication(
     exclude = [UserDetailsServiceAutoConfiguration::class]
+)
+@Import(
+    value = [
+        ComponentScanConfig::class,
+        DomainConfig::class,
+        PersistenceConfig::class
+    ]
 )
 class ApiModuleApplication {
 }
