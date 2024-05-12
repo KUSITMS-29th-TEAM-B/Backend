d9a67595b0f4023f96e171aed61a524c2970997a
seeunjang <wisjang1@naver.com>
2024-05-12 13:31:18 +0900

    refactor : 예외처리 응답 형식 변경, Internal Server Error 예외 처리 추가(#11)

--- Common-Module/src/main/kotlin/com/bamyanggang/commonmodule/exception/ExceptionHandler.kt
+++ Common-Module/src/main/kotlin/com/bamyanggang/commonmodule/exception/ExceptionHandler.kt
@@ -1,13 +1,23 @@
 package com.bamyanggang.commonmodule.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class ExceptionHandler {
     @ExceptionHandler(CustomException::class)
-    fun handleJwtException(e: CustomException): ErrorResponse{
-        return ErrorResponse(e.code, e.message, e.httpStatusCode)
+    fun handleJwtException(e: CustomException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(ErrorResponse(e.code, e.message), e.httpStatusCode)
     }
 
+    @ExceptionHandler(Exception::class)
+    fun handleException(exception: Exception): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(
+            ErrorResponse("INTERNAL_SERVER_ERROR", "Internal Server Error"),
+            HttpStatus.INTERNAL_SERVER_ERROR
+        )
+    }
 }
+
