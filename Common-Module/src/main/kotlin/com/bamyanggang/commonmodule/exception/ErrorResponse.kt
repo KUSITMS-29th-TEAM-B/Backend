93cd5c2d81507f7983036f42335b16591ea40074
seeunjang <wisjang1@naver.com>
2024-05-12 13:32:44 +0900

    refactor : ErrorResponse 필드 수정(#11)

--- Common-Module/src/main/kotlin/com/bamyanggang/commonmodule/exception/ErrorResponse.kt
+++ Common-Module/src/main/kotlin/com/bamyanggang/commonmodule/exception/ErrorResponse.kt
@@ -1,9 +1,6 @@
 package com.bamyanggang.commonmodule.exception
 
-import org.springframework.http.HttpStatusCode
-
 data class ErrorResponse(
     val code: String,
-    val message: String,
-    val httpStatusCode: HttpStatusCode
+    val message: String
 )
