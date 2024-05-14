fa906e524f7bb7c36a9f6979f3869df37f5ef079
whereami0404 <whereami2048@gmail.com>
2024-05-14 16:53:42 +0900

    feat: 태그, 경험, 경험 내용, 역량 키워드 Mapper 클래스 구현(#25)

---
+++ Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/experience/mapper/StrongPointMapper.kt
@@ -0,0 +1,22 @@
+package com.bamyanggang.persistence.experience.mapper
+
+import com.bamyanggang.domainmodule.domain.experience.aggregate.StrongPoint
+import com.bamyanggang.persistence.experience.jpa.entity.StrongPointJpaEntity
+import org.springframework.stereotype.Component
+
+@Component
+class StrongPointMapper {
+    fun toJpaEntity(strongPoint: StrongPoint) =
+        StrongPointJpaEntity.of(
+            strongPoint.id,
+            strongPoint.name,
+            strongPoint.userId
+        )
+
+    fun toDomain(strongPointJpaEntity: StrongPointJpaEntity) =
+        StrongPoint(
+            strongPointJpaEntity.id,
+            strongPointJpaEntity.name,
+            strongPointJpaEntity.userId
+        )
+}
