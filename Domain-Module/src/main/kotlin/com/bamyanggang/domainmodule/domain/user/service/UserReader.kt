71dc37aeca6abaa2fb2362c513af15d3d5b22193
seeunjang <wisjang1@naver.com>
2024-05-15 04:26:49 +0900

    feat : 사용자 정보 추출 Util 클래스 생성(#35)

--- Domain-Module/src/main/kotlin/com/bamyanggang/domainmodule/domain/user/service/UserReader.kt
+++ Domain-Module/src/main/kotlin/com/bamyanggang/domainmodule/domain/user/service/UserReader.kt
@@ -3,6 +3,7 @@ package com.bamyanggang.domainmodule.domain.user.service
 import com.bamyanggang.domainmodule.domain.user.aggregate.User
 import com.bamyanggang.domainmodule.domain.user.repository.UserRepository
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class UserReader(
@@ -10,5 +11,9 @@ class UserReader(
 ) {
     fun readUserBySocialId(socialId: String): User? {
         return userRepository.findBySocialId(socialId)
+    }
+
+    fun readUserById(userId: UUID): User {
+        return userRepository.findById(userId)
     }
 }
