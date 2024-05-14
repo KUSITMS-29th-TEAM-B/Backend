4375f5315ae90796beccdc50f58cd940a34b63da
whereami0404 <whereami2048@gmail.com>
2024-05-14 13:28:42 +0900

    refactor: 경험, 경험 내용 연관관계 객체로 변경(#25)

--- Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/experience/jpa/entity/ExperienceTagJpaEntity.java
+++ Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/experience/jpa/entity/ExperienceTagJpaEntity.java
@@ -1,23 +1,34 @@
 package com.bamyanggang.persistence.experience.jpa.entity;
 
-import static jakarta.persistence.FetchType.*;
+import static jakarta.persistence.FetchType.LAZY;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "experience_tag")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ExperienceTagJpaEntity {
     @Id
+    @Column(name = "experience_tag_id")
     private UUID id;
 
-    private UUID experienceId;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "experience_id")
+    private ExperienceJpaEntity experience;
 
-    private UUID tagId;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "tag_id")
+    private TagJpaEntity tag;
 }
