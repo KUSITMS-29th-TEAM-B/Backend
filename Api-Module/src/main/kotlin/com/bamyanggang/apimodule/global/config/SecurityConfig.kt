68a718ba041d913a4c00a162ced38a456a674036
whereami0404 <whereami2048@gmail.com>
2024-05-14 17:30:11 +0900

    chore: import문 정리 및 변수 선언 위치 변경(#25)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/global/config/SecurityConfig.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/global/config/SecurityConfig.kt
@@ -8,13 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 
 @Configuration
