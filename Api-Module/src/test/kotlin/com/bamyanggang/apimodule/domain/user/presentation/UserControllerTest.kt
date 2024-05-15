d0738767923b21766e0c72df81e150aff86f5592
seeunjang <wisjang1@naver.com>
2024-05-15 19:05:51 +0900

    test : 사용자 정보 수정 API 테스트 코드 작성, 명세 반영(#48)

--- Api-Module/src/test/kotlin/com/bamyanggang/apimodule/domain/user/presentation/UserControllerTest.kt
+++ Api-Module/src/test/kotlin/com/bamyanggang/apimodule/domain/user/presentation/UserControllerTest.kt
@@ -3,10 +3,11 @@ package com.bamyanggang.apimodule.domain.user.presentation
 import com.bamyanggang.apimodule.BaseRestDocsTest
 import com.bamyanggang.apimodule.domain.user.application.dto.ProfileImageResponse
 import com.bamyanggang.apimodule.domain.user.application.dto.Register
-import com.bamyanggang.apimodule.domain.user.application.dto.UserInfoResponse
+import com.bamyanggang.apimodule.domain.user.application.dto.UserInfo
 import com.bamyanggang.apimodule.domain.user.application.service.ProfileImageGetService
 import com.bamyanggang.apimodule.domain.user.application.service.UserCreateService
 import com.bamyanggang.apimodule.domain.user.application.service.UserInfoGetService
+import com.bamyanggang.apimodule.domain.user.application.service.UserInfoUpdateService
 import com.bamyanggang.commonmodule.fixture.generateFixture
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -27,6 +28,8 @@ class UserControllerTest : BaseRestDocsTest(){
         private lateinit var profileImageGetService: ProfileImageGetService
         @MockBean
         private lateinit var userInfoGetService: UserInfoGetService
+        @MockBean
+        private lateinit var userInfoUpdateService: UserInfoUpdateService
 
         @Test
         @DisplayName("회원가입을 진행한다.")
@@ -86,7 +89,7 @@ class UserControllerTest : BaseRestDocsTest(){
         @DisplayName("사용자 정보를 가져온다.")
         fun getUserInfo() {
             //given
-            val userInfoResponse: UserInfoResponse = generateFixture()
+            val userInfoResponse: UserInfo.Response.Success = generateFixture()
             given(userInfoGetService.getUserInfo()).willReturn(userInfoResponse)
             val request = RestDocumentationRequestBuilders.get(UserApi.USER_INFO)
             //when
@@ -106,4 +109,30 @@ class UserControllerTest : BaseRestDocsTest(){
                     )
                 )
         }
+
+    @Test
+    @DisplayName("사용자 정보를 수정한다.")
+    fun updateUserInfo() {
+        //given
+        val userInfoUpdateRequest: UserInfo.Request.UpdateUserInfo = generateFixture()
+        val request = RestDocumentationRequestBuilders.patch(UserApi.USER_INFO_UPDATE)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(objectMapper.writeValueAsString(userInfoUpdateRequest))
+        //when
+        val result = mockMvc.perform(request)
+        //then
+        result.andExpect(status().isOk)
+            .andDo(
+                resultHandler.document(
+                    requestFields(
+                        fieldWithPath("nickName").description("닉네임"),
+                        fieldWithPath("profileImgUrl").description("프로필 이미지 URL"),
+                        fieldWithPath("jobSearchStatus").description("구직 상태"),
+                        fieldWithPath("desiredJob").description("희망 직무"),
+                        fieldWithPath("goal").description("목표"),
+                        fieldWithPath("dream").description("꿈")
+                    )
+            )
+        )
+    }
 }
