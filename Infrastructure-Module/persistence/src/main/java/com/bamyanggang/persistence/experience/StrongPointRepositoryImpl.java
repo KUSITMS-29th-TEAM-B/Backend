13894590fd4ed2046d56dbf0cf31b3154b2e41f8
whereami0404 <whereami2048@gmail.com>
2024-05-15 16:54:57 +0900

    feat: 역량 키워드 Repositoy 인터페이스 및 구현체 클래스 구현(#46)

---
+++ Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/experience/StrongPointRepositoryImpl.java
@@ -0,0 +1,39 @@
+package com.bamyanggang.persistence.experience;
+
+import com.bamyanggang.domainmodule.domain.experience.repository.StrongPointRepository;
+import com.bamyanggang.domainmodule.domain.strongpoint.aggregate.StrongPoint;
+import com.bamyanggang.persistence.experience.jpa.entity.StrongPointJpaEntity;
+import com.bamyanggang.persistence.experience.jpa.repository.StrongPointJpaRepository;
+import com.bamyanggang.persistence.experience.mapper.StrongPointMapper;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class StrongPointRepositoryImpl implements StrongPointRepository {
+    private final StrongPointJpaRepository strongPointJpaRepository;
+    private final StrongPointMapper strongPointMapper;
+
+    @Override
+    public UUID save(StrongPoint strongPoint) {
+        StrongPointJpaEntity strongPointJpaEntity = strongPointMapper.toJpaEntity(strongPoint);
+        strongPointJpaRepository.save(strongPointJpaEntity);
+
+        return strongPointJpaEntity.getId();
+    }
+
+    @Override
+    public List<StrongPoint> findAllByUserId(UUID userId) {
+        List<StrongPointJpaEntity> strongPoints = strongPointJpaRepository.findAllByUserId(userId);
+
+        return strongPoints.stream().map(strongPointEntity ->
+                    StrongPoint.Companion.toDomain(
+                        strongPointEntity.getId(),
+                        strongPointEntity.getName(),
+                        strongPointEntity.getUserId()
+                    )
+            ).toList();
+    }
+}
--- Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/experience/jpa/repository/StrongPointJpaRepository.java
+++ Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/experience/jpa/repository/StrongPointJpaRepository.java
@@ -1,8 +1,10 @@
 package com.bamyanggang.persistence.experience.jpa.repository;
 
 import com.bamyanggang.persistence.experience.jpa.entity.StrongPointJpaEntity;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StrongPointJpaRepository extends JpaRepository<StrongPointJpaEntity, UUID> {
+    List<StrongPointJpaEntity> findAllByUserId(UUID userId);
 }
