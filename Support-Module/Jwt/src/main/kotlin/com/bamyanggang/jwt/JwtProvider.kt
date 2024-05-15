3c388f141916d7e129d0bde52492b1038b1b8fcf
seeunjang <wisjang1@naver.com>
2024-05-16 05:24:03 +0900

    feat : 토큰 재발급 로직 구현, 예외처리(#54)

--- Support-Module/Jwt/src/main/kotlin/com/bamyanggang/jwt/JwtProvider.kt
+++ Support-Module/Jwt/src/main/kotlin/com/bamyanggang/jwt/JwtProvider.kt
@@ -1,18 +1,19 @@
 package com.bamyanggang.jwt
 
+import com.bamyanggang.cache.CacheManager
+import com.bamyanggang.jwt.exception.JwtException
+import com.bamyanggang.lock.LockManager
 import io.jsonwebtoken.Jwts
 import org.springframework.stereotype.Component
 import java.util.*
 
-/**
- * Todo : 토큰 캐싱 처리 추가
- */
 @Component
 class JwtProvider(
     private val jwtProperties: JwtProperties,
     private val jwtKeyGenerator: JwtKeyGenerator,
     private val claimsExtractor: ClaimsExtractor,
     private val jwtValidator: JwtValidator,
+    private val jwtRegistry: JwtRegistry
 ) {
 
     fun generateAccessToken(user: Claims.UserClaims): String {
@@ -36,16 +37,25 @@ class JwtProvider(
         )
     }
 
-    fun reissueToken(refreshToken: String) : Token {
-        jwtValidator.validateToken(refreshToken, TokenType.REFRESH_TOKEN)
-        val userClaims: Claims.UserClaims = claimsExtractor.extractClaimsFromToken(
-            refreshToken, TokenType.REFRESH_TOKEN,
-            JwtConst.USER_CLAIMS, Claims.UserClaims::class.java
-        )
-        val accessToken = generateAccessToken(userClaims)
-        val newRefreshToken = generateRefreshToken(userClaims)
-        return Token(accessToken, newRefreshToken)
+    fun reissueToken(refreshToken: String): Pair<String, String> = LockManager.lockByKey(refreshToken) {
+        CacheManager.cacheByKey(refreshToken) {
+            if (jwtRegistry.isExists(refreshToken).not()) {
+                throw JwtException.TokenNotFoundException()
+            }
+            jwtValidator.validateToken(refreshToken, TokenType.REFRESH_TOKEN)
+            val userClaims: Claims.UserClaims = claimsExtractor.extractClaimsFromToken(
+                refreshToken, TokenType.REFRESH_TOKEN,
+                JwtConst.USER_CLAIMS, Claims.UserClaims::class.java
+            )
+            val accessToken = generateAccessToken(userClaims)
+            val newRefreshToken = generateRefreshToken(userClaims)
+
+            jwtRegistry.upsert(userClaims.userId to newRefreshToken)
+
+            return@cacheByKey Pair(accessToken, newRefreshToken)
+        }
     }
+
 
     private fun generateToken(claims: PrivateClaims, expireTime: Long): String {
         val now = Date()
