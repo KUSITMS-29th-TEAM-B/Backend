c75f8bea3d53353055eb68a8d59865f62f6f68dd
seeunjang <wisjang1@naver.com>
2024-05-15 18:58:29 +0900

    refactor : 사용자 id 추출 유틸리티 함수 사용으로 변경(#48)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/common/UserSecurityUtil.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/common/UserSecurityUtil.kt
@@ -3,8 +3,6 @@ package com.bamyanggang.apimodule.common
 import org.springframework.security.core.context.SecurityContextHolder
 import java.util.UUID
 
-object SecurityUtils {
-    fun getAuthenticationPrincipal(): UUID {
+ fun getAuthenticationPrincipal(): UUID {
         return SecurityContextHolder.getContext().authentication.principal as UUID
-    }
 }
