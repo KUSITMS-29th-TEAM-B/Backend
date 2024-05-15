e6478886081356a2ed7000e4ba500bbaa92ad9c1
seeunjang <wisjang1@naver.com>
2024-05-16 05:41:43 +0900

    refactor : 재발급 API PUT으로 변경(#54)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/user/presentation/AuthController.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/user/presentation/AuthController.kt
@@ -8,6 +8,7 @@ import com.bamyanggang.domainmodule.domain.user.enums.SocialLoginProvider
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
@@ -22,7 +23,7 @@ class AuthController(
         @RequestBody request: SocialLogin.Request
     ): SocialLogin.Response = authService.executeSocialLogin(provider,request)
 
-    @PostMapping(AuthApi.REISSUE)
+    @PutMapping(AuthApi.REISSUE)
     fun reissueToken(
         @RequestBody request: Reissue.Request
     ): Reissue.Response = authService.reissueToken(request)
