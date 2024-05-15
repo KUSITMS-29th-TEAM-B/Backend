ed54baa657a8dc3d0d22a762918f01c683df125e
seeunjang <wisjang1@naver.com>
2024-05-15 18:59:18 +0900

    refactor : 사용자 id 방식 변경으로 인한 사용자 정보 조회 API 로직 수정(#48)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/user/application/service/UserInfoGetService.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/user/application/service/UserInfoGetService.kt
@@ -1,23 +1,26 @@
 package com.bamyanggang.apimodule.domain.user.application.service
 
-import com.bamyanggang.apimodule.common.UserUtils
-import com.bamyanggang.apimodule.domain.user.application.dto.UserInfoResponse
+import com.bamyanggang.apimodule.common.getAuthenticationPrincipal
+import com.bamyanggang.apimodule.domain.user.application.dto.UserInfo
+import com.bamyanggang.domainmodule.domain.user.service.UserReader
 import org.springframework.stereotype.Service
 
 @Service
 class UserInfoGetService(
-    private val userUtils: UserUtils
+    private val userReader: UserReader
 ) {
-
-    fun getUserInfo(): UserInfoResponse {
-        val user = userUtils.getAccessUser()
-        return UserInfoResponse(
-            nickName = user.nickName,
-            profileImgUrl = user.profileImgUrl,
-            jobSearchStatus = user.jobSearchStatus,
-            desiredJob = user.desiredJob,
-            goal = user.goal,
-            dream = user.dream
-        )
+    fun getUserInfo(): UserInfo.Response.Success {
+        getAuthenticationPrincipal()
+            .let { userReader.readUserById(it) }
+            .let {
+                return UserInfo.Response.Success(
+                    nickName = it.nickName,
+                    profileImgUrl = it.profileImgUrl,
+                    jobSearchStatus = it.jobSearchStatus,
+                    desiredJob = it.desiredJob,
+                    goal = it.goal,
+                    dream = it.dream
+                )
+            }
     }
 }
