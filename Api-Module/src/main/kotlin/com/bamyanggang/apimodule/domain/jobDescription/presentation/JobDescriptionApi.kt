c9229acc803b145ad2a28600d4395e7cb3ba78a8
seeunjang <wisjang1@naver.com>
2024-05-17 02:15:10 +0900

    feat : JD 등록 API 스펙 정의(#56)

---
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/jobDescription/presentation/JobDescriptionApi.kt
@@ -0,0 +1,5 @@
+package com.bamyanggang.apimodule.domain.jobDescription.presentation
+
+object JobDescriptionApi {
+    const val BASE_URL = "/api/job-description"
+}
---
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/jobDescription/presentation/JobDescriptionController.kt
@@ -0,0 +1,18 @@
+package com.bamyanggang.apimodule.domain.jobDescription.presentation
+
+import com.bamyanggang.apimodule.domain.jobDescription.application.dto.CreateJobDescription
+import com.bamyanggang.apimodule.domain.jobDescription.application.service.JobDescriptionCreateService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class JobDescriptionController(
+    private val jobDescriptionCreateService: JobDescriptionCreateService,
+) {
+
+    @PostMapping(JobDescriptionApi.BASE_URL)
+    fun createJobDescription(
+        @RequestBody request: CreateJobDescription.Request
+    ) = jobDescriptionCreateService.createJobDescription(request)
+}
