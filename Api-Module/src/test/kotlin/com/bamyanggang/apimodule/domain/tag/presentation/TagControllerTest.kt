5551eb7efd36e4e3a0a1cbbd5968543bd2d65242
whereami0404 <whereami2048@gmail.com>
2024-05-17 04:46:44 +0900

    test: 태그 이름 중복 등록 예외 테스트 코드 작성(#52)

--- Api-Module/src/test/kotlin/com/bamyanggang/apimodule/domain/tag/presentation/TagControllerTest.kt
+++ Api-Module/src/test/kotlin/com/bamyanggang/apimodule/domain/tag/presentation/TagControllerTest.kt
@@ -2,12 +2,15 @@ package com.bamyanggang.apimodule.domain.tag.presentation
 
 import com.bamyanggang.apimodule.BaseRestDocsTest
 import com.bamyanggang.apimodule.domain.tag.application.dto.CreateTag
+import com.bamyanggang.commonmodule.exception.ExceptionHandler
 import com.bamyanggang.commonmodule.fixture.generateFixture
+import com.bamyanggang.domainmodule.domain.tag.exception.TagException
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito.given
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders
 import org.springframework.restdocs.payload.PayloadDocumentation.*
@@ -17,6 +20,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.util.*
 
 @WebMvcTest(TagController::class)
+@Import(ExceptionHandler::class)
 class TagControllerTest : BaseRestDocsTest() {
 
     @MockBean
@@ -80,6 +84,62 @@ class TagControllerTest : BaseRestDocsTest() {
                 ),
                 responseFields(
                     fieldWithPath("id").description("생성된 태그 id")
+                )
+            )
+        )
+    }
+
+    @Test
+    @DisplayName("등록하려는 상위 태그 이름이 유저가 이미 등록하여 갖고 있는 경우 저장되지 않고 예외를 반환한다.")
+    fun duplicatedParentTagTest() {
+        //given
+        val createParentTagRequest: CreateTag.Request = generateFixture()
+
+        val request = RestDocumentationRequestBuilders.post(TagApi.BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(objectMapper.writeValueAsString(createParentTagRequest))
+
+        given(tagController.createTag(null, createParentTagRequest)).willThrow(TagException.DuplicatedTagName())
+
+        //when
+        val result = mockMvc.perform(request)
+
+        //then
+        result.andExpect(status().isBadRequest)
+            .andDo(resultHandler.document(
+                responseFields(
+                    fieldWithPath("code").description(TagException.DuplicatedTagName().code),
+                    fieldWithPath("message").description(TagException.DuplicatedTagName().message)
+                )
+            )
+        )
+    }
+
+    @Test
+    @DisplayName("등록하려는 하위 태그 이름이 유저가 이미 등록하여 갖고 있는 경우 저장되지 않고 예외를 반환한다.")
+    fun duplicatedChildTagTest() {
+        //given
+        val createChildTagRequest: CreateTag.Request = generateFixture()
+        val parentTagId: UUID  = generateFixture()
+
+        val request = RestDocumentationRequestBuilders.post(TagApi.TAG_PATH_VARIABLE_URL, parentTagId)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(objectMapper.writeValueAsString(createChildTagRequest))
+
+        given(tagController.createTag(parentTagId, createChildTagRequest)).willThrow(TagException.DuplicatedTagName())
+
+        //when
+        val result = mockMvc.perform(request)
+
+        //then
+        result.andExpect(status().isBadRequest)
+            .andDo(resultHandler.document(
+                pathParameters(
+                    parameterWithName("tagId").description("상위 태그 id (생략 시 상위 태그 생성 API 호출)")
+                ),
+                responseFields(
+                    fieldWithPath("code").description(TagException.DuplicatedTagName().code),
+                    fieldWithPath("message").description(TagException.DuplicatedTagName().message)
                 )
             )
         )
