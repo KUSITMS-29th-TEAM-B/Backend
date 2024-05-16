e374564d65973e15b8ab6eedc67ef1b383fc064b
whereami0404 <whereami2048@gmail.com>
2024-05-17 03:40:26 +0900

    feat: 태그 삭제 API 스펙 정의(#52)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/tag/presentation/TagApi.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/tag/presentation/TagApi.kt
@@ -2,5 +2,5 @@ package com.bamyanggang.apimodule.domain.tag.presentation
 
 object TagApi {
     const val BASE_URL = "/api/tags"
-    const val CREATE_TAG = "$BASE_URL/{parentTagId}"
+    const val TAG_PATH_VARIABLE_URL = "$BASE_URL/{tagId}"
 }
--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/tag/presentation/TagController.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/tag/presentation/TagController.kt
@@ -1,24 +1,29 @@
 package com.bamyanggang.apimodule.domain.tag.presentation
 
 import com.bamyanggang.apimodule.domain.tag.application.dto.CreateTag
+import com.bamyanggang.apimodule.domain.tag.application.dto.DeleteTag
 import com.bamyanggang.apimodule.domain.tag.application.service.TagCreateService
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 class TagController(
     private val tagCreateService: TagCreateService,
 ) {
-    @PostMapping(TagApi.CREATE_TAG, TagApi.BASE_URL)
-    fun createTag( @PathVariable("parentTagId", required = false) parentTagId: UUID?,
-                   @RequestBody request: CreateTag.Request,
-                  ): CreateTag.Response {
+    @PostMapping(TagApi.BASE_URL, TagApi.TAG_PATH_VARIABLE_URL)
+    fun createTag(
+        @PathVariable("tagId", required = false) parentTagId: UUID?,
+        @RequestBody request: CreateTag.Request,
+    ): CreateTag.Response {
         return when {
             parentTagId == null -> tagCreateService.createParentTag(request)
             else -> tagCreateService.createChildTag(request, parentTagId)
         }
     }
+
+    @DeleteMapping(TagApi.TAG_PATH_VARIABLE_URL)
+    fun deleteTag( @PathVariable("tagId", required = false) tagId: UUID) : DeleteTag.Response {
+
+    }
+
 }
