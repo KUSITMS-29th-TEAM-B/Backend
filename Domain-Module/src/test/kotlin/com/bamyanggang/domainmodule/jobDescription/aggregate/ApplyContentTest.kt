fde65f3db486997742405a48601edfe776eabc88
seeunjang <wisjang1@naver.com>
2024-05-17 16:07:04 +0900

    test : 자소서 등록 API domain, application 계층 테스트 코드 작성(#56)

---
+++ Domain-Module/src/test/kotlin/com/bamyanggang/domainmodule/jobDescription/aggregate/ApplyContentTest.kt
@@ -0,0 +1,46 @@
+package com.bamyanggang.domainmodule.jobDescription.aggregate
+
+import com.bamyanggang.commonmodule.fixture.generateFixture
+import com.bamyanggang.domainmodule.domain.jobDescription.aggregate.ApplyContent
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import java.util.*
+
+class ApplyContentTest : FunSpec({
+
+    test("ApplyContent 생성") {
+        // arrange
+        val question: String = generateFixture()
+        val answer: String = generateFixture()
+        val applyId: UUID = generateFixture()
+
+        // act
+        val applyContent = ApplyContent.create(
+            question = question,
+            answer = answer,
+            applyId = applyId
+        )
+
+        // assert
+        applyContent.question shouldBe question
+        applyContent.answer shouldBe answer
+    }
+
+    test("applyContent 입력값으로 공백이 들어오면 에러 반환") {
+        // arrange
+        val question: String = ""
+        val answer: String = ""
+        val applyId: UUID = generateFixture()
+
+        // act, assert
+        shouldThrow<IllegalArgumentException> {
+            ApplyContent.create(
+                question = question,
+                answer = answer,
+                applyId = applyId
+            )
+        }
+    }
+
+})
