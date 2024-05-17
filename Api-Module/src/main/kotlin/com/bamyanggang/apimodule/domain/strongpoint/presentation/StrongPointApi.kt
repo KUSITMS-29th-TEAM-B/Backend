8d95f71169258f22d8dd25f790ba64eeede2bf7f
whereami0404 <whereami2048@gmail.com>
2024-05-17 11:25:51 +0900

    feat: 역량 키워드 삭제 API 스펙 정의(#47)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/strongpoint/presentation/StrongPointApi.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/strongpoint/presentation/StrongPointApi.kt
@@ -2,4 +2,5 @@ package com.bamyanggang.apimodule.domain.strongpoint.presentation
 
 object StrongPointApi {
     const val BASE_URL = "/api/strong-points"
+    const val STRONG_POINT_PATH_VARIABLE_URL = "$BASE_URL/{strongPointId}"
 }
--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/strongpoint/presentation/StrongPointController.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/strongpoint/presentation/StrongPointController.kt
@@ -2,9 +2,8 @@ package com.bamyanggang.apimodule.domain.strongpoint.presentation
 
 import com.bamyanggang.apimodule.domain.strongpoint.application.dto.CreateStrongPoint
 import com.bamyanggang.apimodule.domain.strongpoint.application.service.StrongPointCreateService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 class StrongPointController(
@@ -13,5 +12,9 @@ class StrongPointController(
     @PostMapping(StrongPointApi.BASE_URL)
     fun createStrongPoint(@RequestBody request: CreateStrongPoint.Request): CreateStrongPoint.Response {
         return strongPointCreateService.createStrongPoint(request)
+    }
+
+    @DeleteMapping(StrongPointApi.STRONG_POINT_PATH_VARIABLE_URL)
+    fun deleteStrongPoint(@PathVariable("strongPointId") strongPointId: UUID) {
     }
 }
