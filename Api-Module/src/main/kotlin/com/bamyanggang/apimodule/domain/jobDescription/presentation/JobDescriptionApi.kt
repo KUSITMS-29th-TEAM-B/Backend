c5aca3b500f64712d9f0a49eac3065a9cc1e6e2c
seeunjang <wisjang1@naver.com>
2024-05-17 15:11:52 +0900

    feat : 자소서 등록 API 스펙 정의(#56)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/jobDescription/presentation/JobDescriptionApi.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/jobDescription/presentation/JobDescriptionApi.kt
@@ -2,4 +2,5 @@ package com.bamyanggang.apimodule.domain.jobDescription.presentation
 
 object JobDescriptionApi {
     const val BASE_URL = "/api/job-description"
+    const val APPLY = "${BASE_URL}/apply/{jobDescriptionId}"
 }
