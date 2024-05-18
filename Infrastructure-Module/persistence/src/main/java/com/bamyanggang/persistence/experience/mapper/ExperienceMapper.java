11892cb87561bff5d3459923c05d132021617f28
whereami0404 <whereami2048@gmail.com>
2024-05-18 22:36:16 +0900

    fix: ExperienceContentMapper 제거(#40)

--- Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/experience/mapper/ExperienceMapper.java
+++ Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/experience/mapper/ExperienceMapper.java
@@ -1,14 +1,14 @@
 package com.bamyanggang.persistence.experience.mapper;
 
 import com.bamyanggang.domainmodule.domain.experience.aggregate.Experience;
+import com.bamyanggang.domainmodule.domain.experience.aggregate.ExperienceContent;
+import com.bamyanggang.persistence.experience.jpa.entity.ExperienceContentJpaEntity;
 import com.bamyanggang.persistence.experience.jpa.entity.ExperienceJpaEntity;
-import java.util.List;
-import java.util.UUID;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExperienceMapper {
-    public ExperienceJpaEntity toJpaEntity(Experience experience) {
+    public ExperienceJpaEntity toExperienceJpaEntity(Experience experience) {
         return new ExperienceJpaEntity(
                 experience.getId(),
                 experience.getTitle(),
@@ -20,20 +20,31 @@ public class ExperienceMapper {
         );
     }
 
-    public Experience toDomainEntity(ExperienceJpaEntity experienceJpaEntity, UUID parentTagId, UUID childTagId,
-                                     List<UUID> strongPointIds, List<UUID> experienceContentIds) {
+    public Experience toExperienceDomainEntity(ExperienceJpaEntity experienceJpaEntity) {
         return Experience.Companion.toDomain(
                 experienceJpaEntity.getExperienceId(),
                 experienceJpaEntity.getUserId(),
-                parentTagId,
-                childTagId,
-                strongPointIds,
                 experienceJpaEntity.getTitle(),
-                experienceContentIds,
                 experienceJpaEntity.getStartedAt(),
                 experienceJpaEntity.getEndedAt(),
                 experienceJpaEntity.getCreatedAt(),
                 experienceJpaEntity.getUpdatedAt()
+        );
+    }
+
+    public ExperienceContentJpaEntity toExperienceContentJpaEntity(ExperienceContent experienceContent) {
+        return new ExperienceContentJpaEntity(
+                experienceContent.getId(),
+                experienceContent.getQuestion(),
+                experienceContent.getAnswer()
+        );
+    }
+
+    public ExperienceContent toExperienceContentDomainEntity(ExperienceContentJpaEntity experienceContentJpaEntity) {
+        return ExperienceContent.Companion.toDomain(
+                experienceContentJpaEntity.getExperienceContentId(),
+                experienceContentJpaEntity.getQuestion(),
+                experienceContentJpaEntity.getAnswer()
         );
     }
 }
