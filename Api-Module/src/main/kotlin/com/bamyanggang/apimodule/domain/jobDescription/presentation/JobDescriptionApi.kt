c00a5f87602aa567bd8068f5f3a379ceb0df2a16
seeunjang <wisjang1@naver.com>
2024-05-20 02:19:25 +0900

    feat : 자기소개서 수정 API 구현(#84)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/jobDescription/presentation/JobDescriptionApi.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/jobDescription/presentation/JobDescriptionApi.kt
@@ -4,5 +4,4 @@ object JobDescriptionApi {
     const val BASE_URL = "/api/job-description"
     const val APPLY = "${BASE_URL}/apply/{jobDescriptionId}"
     const val DETAIL = "${BASE_URL}/{jobDescriptionId}"
-
 }
