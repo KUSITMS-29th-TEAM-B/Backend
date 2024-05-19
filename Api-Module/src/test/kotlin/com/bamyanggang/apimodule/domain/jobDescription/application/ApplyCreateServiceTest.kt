467fd14fdc3a6a2d401a3877359e7818437d2aca
seeunjang <wisjang1@naver.com>
2024-05-19 17:27:55 +0900

    test : 응답값에 의한 JD조회 API 수정, ApplyCreateServiceTest 테스트 코드 수정(#78)

--- Api-Module/src/test/kotlin/com/bamyanggang/apimodule/domain/jobDescription/application/ApplyCreateServiceTest.kt
+++ Api-Module/src/test/kotlin/com/bamyanggang/apimodule/domain/jobDescription/application/ApplyCreateServiceTest.kt
@@ -4,38 +4,68 @@ import com.bamyanggang.apimodule.domain.jobDescription.application.dto.CreateApp
 import com.bamyanggang.apimodule.domain.jobDescription.application.service.ApplyCreateService
 import com.bamyanggang.commonmodule.fixture.generateFixture
 import com.bamyanggang.domainmodule.domain.jobDescription.aggregate.Apply
+import com.bamyanggang.domainmodule.domain.jobDescription.aggregate.JobDescription
+import com.bamyanggang.domainmodule.domain.jobDescription.enums.WriteStatus
 import com.bamyanggang.domainmodule.domain.jobDescription.service.ApplyAppender
 import com.bamyanggang.domainmodule.domain.jobDescription.service.ApplyContentAppender
+import com.bamyanggang.domainmodule.domain.jobDescription.service.JobDescriptionModifier
+import com.bamyanggang.domainmodule.domain.jobDescription.service.JobDescriptionReader
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import java.time.LocalDateTime
 import java.util.*
+
 
 
 class ApplyCreateServiceTest : BehaviorSpec({
     val applyAppender = mockk<ApplyAppender>()
     val applyContentAppender = mockk<ApplyContentAppender>()
-    val applyCreateService = ApplyCreateService(applyAppender, applyContentAppender)
+    val jobDescriptionReader = mockk<JobDescriptionReader>()
+    val jobDescriptionModifier = mockk<JobDescriptionModifier>()
+    val applyCreateService = ApplyCreateService(applyAppender, applyContentAppender, jobDescriptionReader, jobDescriptionModifier)
 
-    given("ApplyCreateService.createApply") {
-        `when`("request가 주어지면") {
-            val request: CreateApply.Request = generateFixture()
-            val jobDescriptionId: UUID = generateFixture()
-            val apply: Apply = generateFixture()
-            every { applyAppender.appendApply(any()) } returns apply
-            every { applyContentAppender.appendApplyContent(any(), any(), any()) } returns Unit
+    Given("CreateApply.Request 와 jobDescriptionId이 들어온 경우") {
+        val request = generateFixture<CreateApply.Request>()
+        val jobDescriptionId = UUID.randomUUID()
+        val jobDescription : JobDescription = generateFixture{
+            it.set("id", jobDescriptionId)
+            it.set("enterpriseName", "기업 이름")
+            it.set("title", "직무 공고 제목")
+            it.set("content", "직무 공고 내용")
+            it.set("link", "직무 공고 링크")
+            it.set("startedAt", LocalDateTime.now())
+            it.set("endedAt", LocalDateTime.now().plusDays(1))
+        }
+        val apply = generateFixture<Apply>()
 
+        every { jobDescriptionReader.readJobDescriptionById(jobDescriptionId) } returns jobDescription
+        every { applyAppender.appendApply(jobDescriptionId) } returns apply
+        every { applyContentAppender.appendApplyContent(any(), any(), any()) } returns Unit
+        every { jobDescriptionModifier.modifyWriteStatus(any(), any()) } returns Unit
+
+        When("createApply가 호출된다") {
             applyCreateService.createApply(request, jobDescriptionId)
 
-            then("appendApply가 호출된다.") {
-                verify { applyAppender.appendApply( jobDescriptionId) }
+            Then("applyAppender.appendApply가 호출된다") {
+                verify { applyAppender.appendApply(jobDescriptionId) }
             }
 
-            then("appendApplyContent가 호출된다.") {
+            Then("applyContentAppender.appendApplyContent가 호출된다") {
                 request.contents.forEach { content ->
-                    verify { applyContentAppender.appendApplyContent(apply.id, content.question, content.answer) }
+                    verify {
+                        applyContentAppender.appendApplyContent(
+                            applyId = apply.id,
+                            question = content.question,
+                            answer = content.answer
+                        )
+                    }
                 }
+            }
+
+            Then("jobDescriptionModifier.modifyWriteStatus 가 호출된다") {
+                verify { jobDescriptionModifier.modifyWriteStatus(jobDescription, WriteStatus.WRITING) }
             }
         }
     }
