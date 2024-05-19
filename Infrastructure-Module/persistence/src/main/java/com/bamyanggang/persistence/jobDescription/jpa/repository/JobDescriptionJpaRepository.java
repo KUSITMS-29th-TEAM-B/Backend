f1e8d766049f32e15fb066594c0bc48d7311aab3
seeunjang <wisjang1@naver.com>
2024-05-19 15:23:51 +0900

    refactor : Apply, JobDescription 도메인 설계 수정에 의한 매핑수정(#78)

--- Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/jobDescription/jpa/repository/JobDescriptionJpaRepository.java
+++ Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/jobDescription/jpa/repository/JobDescriptionJpaRepository.java
@@ -1,10 +1,13 @@
 package com.bamyanggang.persistence.jobDescription.jpa.repository;
 
+import com.bamyanggang.domainmodule.domain.jobDescription.aggregate.JobDescription;
+import com.bamyanggang.domainmodule.domain.jobDescription.enums.WriteStatus;
 import com.bamyanggang.persistence.jobDescription.jpa.entity.JobDescriptionJpaEntity;
 import java.util.UUID;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JobDescriptionJpaRepository extends JpaRepository<JobDescriptionJpaEntity, UUID> {
 
@@ -12,5 +15,42 @@ public interface JobDescriptionJpaRepository extends JpaRepository<JobDescriptio
 
     Slice<JobDescriptionJpaEntity> findAllByUserIdOrderByCreatedAtDesc(UUID userId, Pageable pageable);
 
+    @Query(
+            """
+            SELECT jd FROM JobDescriptionJpaEntity jd
+            LEFT JOIN ApplyJpaEntity a ON jd.jobDescriptionId = a.jobDescriptionId
+            WHERE jd.userId = :userId AND a.writeStatus = :writeStatus
+            ORDER BY jd.createdAt DESC
+            """
+    )
+    Slice<JobDescriptionJpaEntity> findAllByUserIdAndOrderByCreatedAtDescWithApplyWithWriteStatus(UUID userId, WriteStatus writeStatus, Pageable pageable);
+
+    @Query(
+            """
+            SELECT jd FROM JobDescriptionJpaEntity jd
+            LEFT JOIN ApplyJpaEntity a ON jd.jobDescriptionId = a.jobDescriptionId
+            WHERE jd.userId = :userId
+            """
+    )
+    Slice<JobDescriptionJpaEntity> findAllByUserIdWithApply(UUID userId, Pageable pageable);
+
+    @Query(
+            """
+            SELECT jd FROM JobDescriptionJpaEntity jd
+            LEFT JOIN ApplyJpaEntity a ON jd.jobDescriptionId = a.jobDescriptionId
+            WHERE jd.userId = :userId AND a.writeStatus = :writeStatus
+            """
+    )
+    Slice<JobDescriptionJpaEntity> findAllByUserIdWithApplyAndWriteStatus(UUID userId, WriteStatus writeStatus, Pageable pageable);
+
+    @Query(
+            """
+            SELECT jd FROM JobDescriptionJpaEntity jd
+            LEFT JOIN ApplyJpaEntity a ON jd.jobDescriptionId = a.jobDescriptionId
+            WHERE jd.userId = :userId
+            ORDER BY jd.createdAt DESC
+            """
+    )
+    Slice<JobDescriptionJpaEntity> findAllByUserIdAndOrderByCreatedAtDescWithApply(UUID userId, Pageable pageable);
 
 }
