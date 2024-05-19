a69412be5d7cc94d3e3c15284ab0052183da53fe
seeunjang <wisjang1@naver.com>
2024-05-19 12:50:41 +0900

    feat : JD 조회 도메인 리포지토리 생성(#63)

--- Domain-Module/src/main/kotlin/com/bamyanggang/domainmodule/domain/jobDescription/repository/ApplyRepository.kt
+++ Domain-Module/src/main/kotlin/com/bamyanggang/domainmodule/domain/jobDescription/repository/ApplyRepository.kt
@@ -1,8 +1,11 @@
 package com.bamyanggang.domainmodule.domain.jobDescription.repository
 
 import com.bamyanggang.domainmodule.domain.jobDescription.aggregate.Apply
+import java.util.*
 
 interface ApplyRepository {
     fun save(apply: Apply)
+
+    fun findByJobDescriptionId(jobDescriptionId: UUID): Apply?
 
 }
