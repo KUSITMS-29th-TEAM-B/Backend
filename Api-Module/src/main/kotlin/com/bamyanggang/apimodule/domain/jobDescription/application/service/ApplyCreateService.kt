20ae51eecb488ff58e7e776deb2f4e9c3cd4b5ca
seeunjang <wisjang1@naver.com>
2024-05-19 22:43:25 +0900

    refactor : 관련 application 서비스 클래스 수정(#95)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/jobDescription/application/service/ApplyCreateService.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/jobDescription/application/service/ApplyCreateService.kt
@@ -1,9 +1,9 @@
 package com.bamyanggang.apimodule.domain.jobDescription.application.service
 
 import com.bamyanggang.apimodule.domain.jobDescription.application.dto.CreateApply
+import com.bamyanggang.domainmodule.domain.jobDescription.aggregate.ApplyContent
 import com.bamyanggang.domainmodule.domain.jobDescription.enums.WriteStatus
 import com.bamyanggang.domainmodule.domain.jobDescription.service.ApplyAppender
-import com.bamyanggang.domainmodule.domain.jobDescription.service.ApplyContentAppender
 import com.bamyanggang.domainmodule.domain.jobDescription.service.JobDescriptionModifier
 import com.bamyanggang.domainmodule.domain.jobDescription.service.JobDescriptionReader
 import org.springframework.stereotype.Service
@@ -13,24 +13,21 @@ import java.util.*
 @Service
 class ApplyCreateService(
     private val applyAppender: ApplyAppender,
-    private val applyContentAppender: ApplyContentAppender,
     private val jobDescriptionReader: JobDescriptionReader,
     private val jobDescriptionModifier: JobDescriptionModifier
 ) {
     @Transactional
     fun createApply(request: CreateApply.Request, jobDescriptionId: UUID) {
-        jobDescriptionReader.readJobDescriptionById(jobDescriptionId).also {
-            jobDescriptionModifier.modifyWriteStatus(it, WriteStatus.WRITING)
-             applyAppender.appendApply(it.id).also { apply ->
-                 request.contents.forEach { content ->
-                     applyContentAppender.appendApplyContent(
-                         applyId = apply.id,
-                         question = content.question,
-                         answer = content.answer
-                     )
-                 }
-            }
+        val applyContents: List<ApplyContent> = request.contents.map {
+            ApplyContent.create(
+                question = it.question,
+                answer = it.answer
+            )
         }
+
+        val jobDescription = jobDescriptionReader.readJobDescriptionById(jobDescriptionId)
+        jobDescriptionModifier.modifyWriteStatus(jobDescription, WriteStatus.WRITING)
+        applyAppender.appendApply(contents = applyContents, jobDescriptionId = jobDescriptionId)
     }
 
 }
