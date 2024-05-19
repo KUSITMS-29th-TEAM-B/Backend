e8b8a88cfe2591458da4e46830771325327d721b
whereami0404 <whereami2048@gmail.com>
2024-05-19 12:39:58 +0900

    feat: 유저 경험 내 존재 연도 조회 기능 구현(#38)

--- Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/experience/jpa/repository/ExperienceJpaRepository.java
+++ Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/experience/jpa/repository/ExperienceJpaRepository.java
@@ -1,8 +1,10 @@
 package com.bamyanggang.persistence.experience.jpa.repository;
 
 import com.bamyanggang.persistence.experience.jpa.entity.ExperienceJpaEntity;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExperienceJpaRepository extends JpaRepository<ExperienceJpaEntity, UUID> {
+    List<ExperienceJpaEntity> findAllByUserId(UUID userId);
 }
