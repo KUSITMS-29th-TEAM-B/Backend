4aa89ede4768b3602b7560557bffd4d9a460101e
seeunjang <wisjang1@naver.com>
2024-05-20 19:07:27 +0900

    feat : 북마크 API 구현(#107)

---
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/bookmark/presentation/BookmarkApi.kt
@@ -0,0 +1,6 @@
+package com.bamyanggang.apimodule.domain.bookmark.presentation
+
+object BookmarkApi {
+    const val BASE_URL = "/api/bookmark"
+    const val BOOKMARK = "$BASE_URL/{jobDescriptionId}/{experienceId}"
+}
