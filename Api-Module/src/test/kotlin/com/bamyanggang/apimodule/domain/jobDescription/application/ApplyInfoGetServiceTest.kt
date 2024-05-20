5a334be574b2300528e6d2640e4482c27f506888
seeunjang <wisjang1@naver.com>
2024-05-20 21:27:02 +0900

    test : ApplyInfoGetServiceTest 수정(#113)

--- Api-Module/src/test/kotlin/com/bamyanggang/apimodule/domain/jobDescription/application/ApplyInfoGetServiceTest.kt
+++ Api-Module/src/test/kotlin/com/bamyanggang/apimodule/domain/jobDescription/application/ApplyInfoGetServiceTest.kt
@@ -2,14 +2,16 @@ package com.bamyanggang.apimodule.domain.jobDescription.application
 
 import com.bamyanggang.apimodule.domain.jobDescription.application.service.ApplyInfoGetService
 import com.bamyanggang.domainmodule.domain.jobDescription.service.ApplyReader
+import com.bamyanggang.domainmodule.domain.jobDescription.service.JobDescriptionReader
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.mockk
 import io.mockk.verify
 import java.util.UUID
 
-class ApplicationInfoGetServiceTest: BehaviorSpec({
+class ApplyInfoGetServiceTest: BehaviorSpec({
     val mockApplyReader = mockk<ApplyReader>(relaxed = true)
-    val service =  ApplyInfoGetService(mockApplyReader)
+    val mockJobDescriptionReader = mockk<JobDescriptionReader>(relaxed = true)
+    val service =  ApplyInfoGetService(mockApplyReader, mockJobDescriptionReader)
 
     given("ApplyInfoGetService.getApplyInfo") {
         val jobDescriptionId: UUID = UUID.randomUUID()
