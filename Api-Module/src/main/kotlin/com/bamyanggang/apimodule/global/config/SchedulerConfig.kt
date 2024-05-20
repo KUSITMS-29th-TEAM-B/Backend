5ef0cb489a86cd4136698e7662395b6093dfed72
seeunjang <wisjang1@naver.com>
2024-05-21 02:26:08 +0900

    feat : SchedulerConfig 생성(#85)

---
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/global/config/SchedulerConfig.kt
@@ -0,0 +1,9 @@
+package com.bamyanggang.apimodule.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@EnableScheduling
+@Configuration
+class SchedulerConfig {
+}
