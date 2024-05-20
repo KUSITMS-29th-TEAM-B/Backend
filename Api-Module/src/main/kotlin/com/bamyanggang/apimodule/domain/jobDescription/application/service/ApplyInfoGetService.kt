241a045caeb60c341905bbed2caf0b21a6c9bdc0
seeunjang <wisjang1@naver.com>
2024-05-20 15:44:49 +0900

    refactor : 트랜잭션 어노테이션 추가(#86)

--- Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/jobDescription/application/service/ApplyInfoGetService.kt
+++ Api-Module/src/main/kotlin/com/bamyanggang/apimodule/domain/jobDescription/application/service/ApplyInfoGetService.kt
@@ -3,6 +3,7 @@ package com.bamyanggang.apimodule.domain.jobDescription.application.service
 import com.bamyanggang.apimodule.domain.jobDescription.application.dto.ApplyInfo
 import com.bamyanggang.domainmodule.domain.jobDescription.service.ApplyReader
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Service
@@ -10,6 +11,7 @@ class ApplyInfoGetService(
     private val applyReader: ApplyReader
 ) {
 
+    @Transactional(readOnly = true)
     fun getApplyInfo(jobDescriptionId: UUID): ApplyInfo.Response {
         return applyReader.readApplyByJobDescriptionId(jobDescriptionId).contents.map {
             ApplyInfo.ContentInfo(
