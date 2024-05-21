ca22485ee1f5d403a453a6a73ff9324bb073dbd8
seeunjang <wisjang1@naver.com>
2024-05-21 16:01:02 +0900

    refactor : StrongPoint 도메인 서비스 빈 등록 방식 수정(#122)

---
+++ Domain-Module/src/main/kotlin/com/bamyanggang/domainmodule/domain/strongpoint/config/StrongPointServiceConfiguration.kt
@@ -0,0 +1,30 @@
+package com.bamyanggang.domainmodule.domain.strongpoint.config
+
+import com.bamyanggang.domainmodule.domain.strongpoint.repository.StrongPointRepository
+import com.bamyanggang.domainmodule.domain.strongpoint.service.StrongPointAppender
+import com.bamyanggang.domainmodule.domain.strongpoint.service.StrongPointReader
+import com.bamyanggang.domainmodule.domain.strongpoint.service.StrongPointRemover
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class StrongPointServiceConfiguration(
+    private val strongPointRepository: StrongPointRepository
+) {
+
+    @Bean
+    fun strongPointAppender(): StrongPointAppender {
+        return StrongPointAppender(strongPointRepository)
+    }
+
+    @Bean
+    fun strongPointReader(): StrongPointReader {
+        return StrongPointReader(strongPointRepository)
+    }
+
+    @Bean
+    fun strongPointModifier(): StrongPointRemover{
+        return StrongPointRemover(strongPointRepository)
+    }
+
+}
