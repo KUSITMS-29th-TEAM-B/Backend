05eb6e74e2ea6816cefe30f8e5b95765aaa693bd
whereami0404 <whereami2048@gmail.com>
2024-05-23 12:22:51 +0900

    refactor: 역량 키워드 매퍼 클래스 객체 생성 방식 리팩토링 & 도메인 클래스 toDomain 메소드 삭제(#150)

--- Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/strongpoint/jpa/entity/StrongPointJpaEntity.java
+++ Infrastructure-Module/persistence/src/main/java/com/bamyanggang/persistence/strongpoint/jpa/entity/StrongPointJpaEntity.java
@@ -8,14 +8,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "strong_point")
 public class StrongPointJpaEntity {
     @Id
@@ -29,7 +27,9 @@ public class StrongPointJpaEntity {
     @Convert(converter = UUIDBinaryConverter.class)
     private UUID userId;
 
-    public static StrongPointJpaEntity of(UUID id, String name, UUID userId) {
-        return new StrongPointJpaEntity(id, name, userId);
+    public StrongPointJpaEntity (UUID id, String name, UUID userId) {
+        this.strongPointId = id;
+        this.name = name;
+        this.userId = userId;
     }
 }
